ad6faffebee47f08519fea22c5e759a117b89e6c
Amith V <amithv0705@gmail.com>
2025-05-21 15:22:19 +0530

    Completed Home Page basic one as of now 21st May 2025 15:22

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import './NavBar.css';
+import './Navbar.css';
 
-const NavBar = () => {
+const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">CampusConnect</div>
-      <ul className="navbar-links">
-        <li><a href="#features">Features</a></li>
-        <li><a href="#events">Events</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <button className="btn btn-contact">Contact Us</button>
+      <div className="navbar-inner">
+        <div className="navbar-logo">CampusConnect</div>
+
+        <div className="navbar-center">
+          <ul className="navbar-links">
+            <li><a href="#features">Features</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+
+        <div className="navbar-buttons">
+          <button className="btn btn-contact">Contact Us</button>
+          <button className="btn btn-login">Login</button>
+        </div>
+      </div>
     </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
