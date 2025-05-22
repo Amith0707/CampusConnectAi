c7ee204cb599b8bc6d8cc72dfaab0fe985524405
Amith V <amithv0705@gmail.com>
2025-05-23 01:03:05 +0530

    Connected ClubLogin.jsx with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ClubLogin from './pages/clubMember/ClubLogin';
+import ClubDashboard from './pages/clubMember/ClubDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/club-login" element={<ClubLogin />} />
+        <Route path="/club-dashboard/:clubName" element={<ClubDashboard />} />
       </Routes>
     </Router>
   );
